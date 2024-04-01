ac9461f41db25aeebf21c1bee9a5ad4a3c038d61
Ajay Pal <124481153+ajayp9@users.noreply.github.com>
2024-04-01 20:21:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// const countValue = document.querySelector('#counter');
+const countValue = document.getElementById('counter');
+
+const increment = () => {
+    // get the value from ui
+    let value = parseInt(countValue.innerText);
+    //update the value
+    value = value + 1;
+    //set the value onto ui
+    countValue.innerText = value;
+};
+
+const decrement = () => {
+     // get the value from ui
+     let value = parseInt(countValue.innerText);
+     //update the value
+     value = value - 1;
+     //set the value onto ui
+     countValue.innerText = value;
+};
